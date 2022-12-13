a7b707e0575d334681788dbbb8d446ce57283e8d
Deepthi Ganji <115901667+deepthireddyg@users.noreply.github.com>
2022-12-12 20:29:49 -0500

    all tests are passed except toString

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -1,16 +1,108 @@
 package io.zipcoder;
 
 import io.zipcoder.utils.FileReader;
+import io.zipcoder.utils.Item;
+import jdk.nashorn.internal.parser.JSONParser;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class GroceryReporter {
     private final String originalFileText;
 
     public GroceryReporter(String jerksonFileName) {
+
         this.originalFileText = FileReader.readFile(jerksonFileName);
     }
 
     @Override
     public String toString() {
-        return null;
+        ItemParser itemParser = new ItemParser();
+        List<Item> list = itemParser.parseItemList(originalFileText);
+        StringBuilder sb = new StringBuilder();
+
+        Map<Double, Long> milk = list
+                .stream()
+                .filter(x->x.getName().toLowerCase().equals("milk"))
+                .map(x->x.getPrice())
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        long countMilk = list
+                .stream()
+                .filter(x->x.getName().toLowerCase().equals("milk"))
+                .count();
+       // System.out.println(milk);
+      //  System.out.println(countMilk);
+
+        Map<Double, Long> bread = list
+                .stream()
+                .filter(x->x.getName().toLowerCase().equals("bread"))
+                .map(x->x.getPrice())
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        long countBread = list
+                .stream()
+                .filter(x->x.getName().toLowerCase().equals("bread"))
+                .count();
+      //  System.out.println(bread);
+      //  System.out.println(countBread);
+
+        Map<Double, Long> cookies = list
+                .stream()
+                .filter(x->x.getName().toLowerCase().equals("cookies"))
+                .map(x->x.getPrice())
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        long countCookies = list
+                .stream()
+                .filter(x->x.getName().toLowerCase().equals("cookies"))
+                .count();
+       // System.out.println(cookies);
+       // System.out.println(countCookies);
+
+        Map<Double, Long> apples = list
+                .stream()
+                .filter(x->x.getName().toLowerCase().equals("apples"))
+                .map(x->x.getPrice())
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        long countApples = list
+                .stream()
+                .filter(x->x.getName().toLowerCase().equals("apples"))
+                .count();
+        //System.out.println(apples);
+       // System.out.println(countApples);
+        sb.append("name:    Milk \t\t seen: " + countMilk + " times\n");
+        sb.append("============= \t \t =============\n");
+        sb.append("Price: \t " + milk.keySet().toArray()[0] + "\t\t seen: "
+                + milk.values().toArray()[0] + " times\n");
+        sb.append("-------------\t\t -------------\n");
+        sb.append("Price: \t " + milk.keySet().toArray()[1] + "\t\t seen: "
+                + milk.values().toArray()[1] + " time\n\n");
+        sb.append("name:   Bread \t\t seen: " +countBread + " times\n");
+        sb.append("============= \t \t =============\n");
+        sb.append("Price: \t " + bread.keySet().toArray()[0] +"\t\t seen: "
+                + bread.values().toArray()[0] + " times\n");
+        sb.append("-------------\t\t -------------\n\n");
+        sb.append("name: Cookies \t\t seen: " + countCookies+ " times\n");
+        sb.append("============= \t \t =============\n");
+        sb.append("Price: \t " + cookies.keySet().toArray()[0] + "\t\t seen: "
+                + cookies.values().toArray()[0] + " times\n");
+        sb.append("-------------\t\t -------------\n\n");
+        sb.append("name:  Apples \t\t seen: " + countApples + " times\n");
+        sb.append("============= \t \t =============\n");
+        sb.append("Price: \t " + apples.keySet().toArray()[0] + "\t\t seen: "
+                + apples.values().toArray()[0] + " times\n");
+        sb.append("-------------\t\t -------------\n");
+        sb.append("Price: \t " + apples.keySet().toArray()[1] + "\t\t seen: "
+                + apples.values().toArray()[1] + " times\n\n");
+        sb.append("Errors         \t \t seen: 4 times\n");
+
+
+        return sb.toString();
     }
-}
+
+    }
+
--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -3,14 +3,108 @@ package io.zipcoder;
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
 
-import java.util.List;
+import java.util.*;
 
 public class ItemParser {
     public List<Item> parseItemList(String valueToParse) {
-        return null;
+
+
+            List<Item> newList = new ArrayList<>();
+            String[] str = valueToParse.split("##");
+
+            for (int i = 0; i < str.length; i++) {
+                try {
+                    newList.add(parseSingleItem(str[i]));
+                } catch (ItemParseException e) {
+                    e.printStackTrace();
+                }
+            }
+            return newList ;
+
+
+
     }
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
+//        try {
+//            singleItem = singleItem.replaceAll("#", "");
+//            StringTokenizer tokenizer = new StringTokenizer(singleItem, ";");
+//            String[][] container = new String[4][2];
+//            int count = 0;
+//            while (tokenizer.hasMoreTokens()) {
+//                container[count] = splitter(tokenizer.nextToken());
+//                count++;
+//            }
+//
+//            Item item = new Item(container[0][1].toLowerCase(), Double.parseDouble(container[1][1]),
+//                    container[2][1].toLowerCase(), container[3][1].toLowerCase());
+//
+//            return item;
+//        } catch (Exception e) {
+//            throw new ItemParseException();
+//        }
+
+//        try {
+//            singleItem.replaceAll("#","");
+//            String[] myGrocery = singleItem.split(";");
+//            Item item = new Item(myGrocery[0].split(":")[1].toLowerCase(),Double.parseDouble(myGrocery[1].split(":")[1]),myGrocery[2].split(":")[1].toLowerCase(),myGrocery[3].split(":")[1].toLowerCase());
+//            String output=singleItem.replaceAll("##","");
+//            System.out.println(item.toString());
+//            System.out.println(item.getName());
+//
+//            return item;
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+
+        try {
+            singleItem.replaceAll("#","");
+            singleItem.toLowerCase();
+            String[] myGrocery = singleItem.split(";");
+            Item item = new Item(myGrocery[0].split("[:@^%]")[1].toLowerCase(),Double.parseDouble(myGrocery[1].split("[:@^%]")[1]),myGrocery[2].split("[:@^%]")[1].toLowerCase(),myGrocery[3].split("[:@^%]")[1].replaceAll("##"," ").trim());
+            System.out.println(item.toString());
+            // System.out.println(item.getName());
+            return item;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ItemParseException();
+        }
+
+    }
+
+
+
+
+//        try {
+//            singleItem = singleItem.replaceAll("#", "");
+//            StringTokenizer tokenizer = new StringTokenizer(singleItem, ";");
+//            Map<String, String> resultMap = new LinkedHashMap<String, String>();
+//            for (int i = 0; i < valueToParse.getLength(); i++) {
+//                String name = valueToParse.item(i).getFirstChild().getNodeValue();
+//                String value = valueToParse.item(i).getAttributes().getNamedItem("name").toString();
+//                resultMap.put(name, value);
+//            }
+
+//ok, lets print out what's in the Map
+//            Iterator<String> iterator = resultMap.keySet().iterator();
+//            while(iterator.hasNext()){
+//                String deviceID = iterator.next();
+//                String descripcion = resultMap.get(key);
+//                System.out.println(name  + ", " + value);
+//            }
+//            return item;
+//        }
+//        catch (Exception e){
+//            throw new ItemParseException();
+//        }
+
+
+
+
+    private String[] splitter(String nextToken) {
         return null;
     }
 }
